44102771c8c602200095b69506ec1fc725385c70
Sam <sammuigai880@gmail.com>
2025-10-19 15:37:41 +0300

    add status endpoint

---
+++ src/main/kotlin/com/portfolio/backend/controllers/StatusController.kt
@@ -0,0 +1,13 @@
+package com.portfolio.backend.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class StatusController {
+
+    @GetMapping
+    fun status() = "We are live 💪"
+}
